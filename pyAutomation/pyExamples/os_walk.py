840f62691e52d88c2ca720506e314df2445e52c6
sallar248 <sallar248@gmail.com>
2017-12-17 13:42:12 -0700

    Python Stuff

---
+++ pyAutomation/pyExamples/os_walk.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import fnmatch
+import os
+
+image = ['*.jpg', '*.jpeg', '*.png', '*.tif', '*.tiff']
+matches = []
+
+for root, dirnames, fineames in os.walk("C:\"):
+    for extenstions in images:
+        for filenames in fnamtch.filter9filternames, extensions):
+            matches.append(os.path.join(root, filename))
+
