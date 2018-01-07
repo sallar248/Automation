4c288b7790fb481545d0f3829d9bea2813cd6564
sallar248 <sallar248@gmail.com>
2018-01-07 12:40:49 -0700

    Sequence Slicing

---
+++ pyAutomation/pyStuff/6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Sequence Slicing
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print(letters[3:6])
+
+# d has index of 3
+# e has index of 4
+# f has index of 5
+# 6 is upper bound
