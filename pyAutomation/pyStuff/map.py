bebf993a6cc3fcbb2555006d48107d6e081b4258
sallar248 <sallar248@gmail.com>
2018-01-07 13:18:36 -0700

    Map and String Function

---
+++ pyAutomation/pyStuff/map.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Create script tht generates the expected otput using my_range as input data.
+# expected list output are all strings
+
+my_range = range(1, 21)
+print(list(map(str, my_range)))
+
+# Map function, applies a function to interval object.  my_range is an object
+# that you can interate
+# it applies str function, coverts numbers to strings
+
+
