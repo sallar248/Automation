ad9165f08709c0e63e7f3f75b339cec41a6b8873
sallar248 <sallar248@gmail.com>
2018-01-07 13:36:12 -0700

    Accessing values in a list

---
+++ pyAutomation/pyStuff/accessing_key_values.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Dictionary Items Sum up
+# Calculate key values of a and b
+
+d = {"a": 1, "b" : 2, "c": 3}
+print(d["b"] + d["a"])
+
+# Sum of all dictionary values take another approach and access values
+# One by one
