0ae9585dd2309fc15146eeec97e1de33791c4433
sallar248 <sallar248@gmail.com>
2018-01-07 13:40:24 -0700

    Addressing KeyErrors

---
+++ pyAutomation/pyStuff/keyErrors.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Key Errors
+
+d = {"Name": "John", "Surname": "Smith"}
+print (d["Smith"])
+
+# Key named Smith doesnt exist
+# Use surname if you want to access Smith
+# Key Error means python could not find a key with the name shown
+# next to the KeyError
