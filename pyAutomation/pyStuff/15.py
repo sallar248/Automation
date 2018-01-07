f7130a32a12d062257f704a293a8f65d0a9b9bdf
sallar248 <sallar248@gmail.com>
2018-01-07 13:29:23 -0700

    Creating Dictionaries

---
+++ pyAutomation/pyStuff/15.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Simple Directory
+# Create a dictionary that contains the keys a and b and their 
+# respective values 1 and 2
+
+# Curly brackes is one way to create a dictionary
+d = {"a": 1, "b":2}
+print d
+
+# Dict function is another way to create a dictionary
+d = dict(a = 1, b = 2)
+print d
+
+
