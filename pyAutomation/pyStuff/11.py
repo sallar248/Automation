3e5fd4f608901aa27f39dd5ffb159c6eea5ab27d
sallar248 <sallar248@gmail.com>
2018-01-07 13:11:11 -0700

    Range Function

---
+++ pyAutomation/pyStuff/11.py
@@ -0,0 +1,13 @@
+#!/user/bin/python
+# List Ranges
+# Generate a list 1-20 and print out
+# range function
+# list function
+
+my_range = range(1,21)
+print(list(my_range))
+
+# Range() python function that generaes a range of integrers
+# Range creates a python range object
+# To get a real list object you need to use the list() function
+# covert the range object into a list object
