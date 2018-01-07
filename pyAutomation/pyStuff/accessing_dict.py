f2d7829bba6c7e470ca47ca7b462babd5ba82c5d
sallar248 <sallar248@gmail.com>
2018-01-07 13:31:51 -0700

    Accessing Dictionaries

---
+++ pyAutomation/pyStuff/accessing_dict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Accessing Diectionary Items
+# Print value of key b
+
+d = {"a": 1, "b": 2}
+print(d["b"])
+
+# Accessing a dictionary value is the same syntax as accessing list item
+# The difference is that lists have indexes, while dictinaries have
+# keys which you create by yourself
