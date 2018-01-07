ba7bc220c270faa9268d1e5ce93e4ff5b23e1ae4
sallar248 <sallar248@gmail.com>
2018-01-07 12:53:27 -0700

    Negative Indexing

---
+++ pyAutomation/pyStuff/negative_indexing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Negative Indexing
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[-2])
+
+# Left to Right positive indexing system starts from zero
+# Sequence data types such as lists have a second indexing system 
+# starts from -1 nd decreases by one from right to left
+
