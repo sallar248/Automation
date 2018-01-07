f98d3aa15824d77aaa96fd39231bf696d4de9bdc
sallar248 <sallar248@gmail.com>
2018-01-07 12:43:25 -0700

    All

---
+++ pyAutomation/pyStuff/5.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#Print second item from list
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[1])
+
+# List is referenced to an index number starting from zero
+# Increasing by one
