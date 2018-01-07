9ccf3b0aafaf2c68881a32be55199eb2389083ed
sallar248 <sallar248@gmail.com>
2018-01-07 13:22:00 -0700

    Set Function for List

---
+++ pyAutomation/pyStuff/14.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Removing Duplicates
+# Remove dupicates from list 
+
+a = ["1", 1, "1", 2]
+a = list(set(a))
+print(a)
+
+
+# Set function to covert the list to a set which would remove dups
+# Then use list to set back to a list
+
