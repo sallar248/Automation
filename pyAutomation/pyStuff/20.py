d10e05d9ee688a4418045b493416d81c43878f9e
sallar248 <sallar248@gmail.com>
2018-01-07 13:47:37 -0700

    Calc sum of all dictionary values

---
+++ pyAutomation/pyStuff/20.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Applying funcitons to dictionary items
+# Calculate the sum of all dictionary values
+
+d = {"a": 1, "b":2, "c": 3}
+print(sum(d.values()))
+
+# d.values() returns a list-like dict_values object while the sum 
+# function calculates the sum of the dict)values items
+
