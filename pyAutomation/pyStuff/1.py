a8a85290a167d9d22e8298f3d0ebac7c4deefea2
sallar248 <sallar248@gmail.com>
2018-01-07 12:56:10 -0700

    Variable Updating

---
+++ pyAutomation/pyStuff/1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Variable Updating
+
+a = 2
+a = 4
+a = 6
+print(a + a + a)
+
+# Python reads/executes from top to bottom
+# variable a will be updated in every line until where a finally
+# gets a value of 6 
+# 6+6+6 = 18
