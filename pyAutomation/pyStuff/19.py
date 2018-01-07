96581f0304dc204c46fa3c6122608b639892a36c
sallar248 <sallar248@gmail.com>
2018-01-07 13:43:31 -0700

    Adding Key values to dictionary

---
+++ pyAutomation/pyStuff/19.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# Add Dictionary Keys
+
+d = {"a": 1, "b": 2}
+d["c"] = 3
+print(d)
+
+
+# Adding pairs of keys and values is straightforward
+# Can't fix the order of dictionary items
+# Dictionaries are unordered collections of items
