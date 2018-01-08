56c556d07c8716f6c2b9b2a3a5d90853d997686d
sallar248 <sallar248@gmail.com>
2018-01-07 17:44:43 -0700

    Print Dictionary

---
+++ pyAutomation/pyStuff/22.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Create dictionary with keys with values
+# Print out dictionary
+
+
+from pprint import pprint
+d = dict(a = list(range(1,11)), b = list(range(11,21)), c = list(range(21,33)))
+pprint(d)
+
+# Ranges are used to construct the lists
+# Use pprint module which is used ot print out well formatted views of 
+# datatypes in Python
+
