3778e8ce8647f53e4f0e700a542d670dd0db666d
sallar248 <sallar248@gmail.com>
2018-01-07 17:53:17 -0700

    Printing Dict Values

---
+++ pyAutomation/pyStuff/24.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Print the expected Output
+# Print b values, c values, a values
+
+
+d = dict(a =  list(range(1,11)), b = list(range(11,21)), c = list(range(21,31)))
+
+for key, value in d.items():
+    print(key, "has value", value)
