6fe436df5f5795a07fed08ba1ecd9bccb1ab0114
sallar248 <sallar248@gmail.com>
2018-01-07 17:48:25 -0700

    Accessing Value of Dictionary

---
+++ pyAutomation/pyStuff/access_dict_value.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Access value of key b
+
+from pprint import pprint
+d = dict(a = list(range(1, 11)), b = list(range(11,21)), c = list(range(21,31)))
+
+pprint(d)
+pprint(d['b'][2])
+
+
