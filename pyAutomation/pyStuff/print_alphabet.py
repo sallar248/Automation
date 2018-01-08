1d0636c5f10150f39f2320587c4126a27e1ce759
sallar248 <sallar248@gmail.com>
2018-01-07 17:57:54 -0700

    Printing Alphabet with String Module

---
+++ pyAutomation/pyStuff/print_alphabet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Script that prints outs letters of English alphavet from a to z
+
+
+import string
+
+for letter in string.ascii_lowercase:
+    print(letter)
+
+# string module and string.ascii_lowercase returns a string object with all alphas
+# Iterate thru that string and print out the string items
+
+
